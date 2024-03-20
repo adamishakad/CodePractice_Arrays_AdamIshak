a0cf47697c4789b691baea979281b7a9ba8a829b
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-03-19 23:35:23 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CodePractice_Arrays_AdamIshak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            string[] students = new string[5] { "Zach", "Wong", "Hannah", "Kris", "Sam" };
+            Console.WriteLine("Student Names:");
+            for (int i = 0; i < students.Length; i++)
+
+             Console.WriteLine(students[i]);
+
+            int[] numbers = new int[] { 5, 12, 8, 88, 7 };
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) ;
+
+            Console.WriteLine(numbers[sum]);
+
+            Console.WriteLine($"Sum: {sum}");
+            double[] doubles = new double[] { 5.2, 7.9, 10.0, 4, 9.7 };
+            Console.WriteLine(doubles);
+
+           
+           
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }  
+    
+
+
+ }       
+       
+
+
+    
+
+
